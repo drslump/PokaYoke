f1795db9e4a8e17c88e30e62b79a3567a80d2929
DrSlump <imv@tid.es>
2010-01-02 01:30:13 +0100

    the poka yoke name exposed to the templates now can be defined

--- PokaYoke.php
+++ PokaYoke.php
@@ -27,6 +27,28 @@ require_once 'DrSlump/Zend/View/PokaYoke/Proxy.php';
  */
 class DrSlump_Zend_View_PokaYoke extends Zend_View
 {
+    protected $_pokaVarName = 'poka';
+
+    /**
+     * Gets the name used for the poka yoke variable in the templates
+     * 
+     * @return string
+     */
+    public function getPokaYokeName()
+    {
+        return $this->_pokaVarName;
+    }
+
+    /**
+     * Sets a new name for the poka yoke variable in the templates
+     *
+     * @param string $varName
+     */
+    public function setPokaYokeName($varName)
+    {
+        $this->_pokaVarName = $varName;
+    }
+
     /**
      * Includes the view script in a scope with only public $this variables.
      *
@@ -34,8 +56,10 @@ class DrSlump_Zend_View_PokaYoke extends Zend_View
      */
     protected function _run()
     {
+        $varname = $this->_pokaVarName;
+
         // Create a local variable holding the escaped template data
-        $poka = new Tid_Zend_View_PokaYoke_Proxy($this, $this);
+        $$varname = new Tid_Zend_View_PokaYoke_Proxy($this, $this);
 
         if ($this->_useViewStream && $this->useStreamWrapper()) {
             include 'zend.view://' . func_get_arg(0);
